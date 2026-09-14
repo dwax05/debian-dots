ea88145fedc0e198e085d3fe029bb6ec0d87db3a
Dylan Wax <djwax12@gmail.com>
2026-09-14 03:13:56 -0400

    fix: treesitter error patch 2

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -701,7 +701,6 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
@@ -728,6 +727,10 @@ require("lazy").setup({
 			},
 			indent = { enable = true, disable = { "ruby" } },
 		},
+		config = function(_, opts)
+			local ok, configs = pcall(require, "nvim-treesitter.configs")
+			if ok then configs.setup(opts) end
+		end,
 		-- There are additional nvim-treesitter modules that you can use to interact
 		-- with nvim-treesitter. You should go explore a few and see what interests you:
 		--
